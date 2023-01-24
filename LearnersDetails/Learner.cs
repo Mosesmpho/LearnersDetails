ac75c002c1993439c89dfdc7fd2724a7a256ace1
Mosesmpho <mosesmpho16@gmail.com>
2023-01-24 12:58:04 +0200

    Add project files.

---
+++ LearnersDetails/Learner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnersDetails
+{
+     class Learner
+    {
+        public string firstname;
+        public string lastname;
+        public string gender;
+        public int age;
+        public char initial;
+
+        public  Learner (string firstname, string lastname, string gender, int age, char initial)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.gender = gender;
+            this.age = age;
+            this.initial = initial;
+        }
+    }
+}
---
+++ LearnersDetails/Program.cs
@@ -0,0 +1,11 @@
+﻿using LearnersDetails;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Learner learner = new Learner("Mpho", "Mokubedi", "Male", 24, 'M');
+
+        Console.WriteLine(learner.firstname + " " + learner.lastname + " " + learner.gender + " " + learner.age + " " + learner.initial);
+    }
+}
